7ebd19c978175b3ca648ff871b8f28518833b579
Hong Zhen Cao <hong13503063322@gmail.com>
2024-05-19 02:45:19 -0400

    rock paper scissors console --> ui

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+let computerChoice = "";
+let roundNum = 1;
+let winner = "";
 
-function computerChoice() {
+function generateComputerChoice() {
   let randomNum = 0;
   while (randomNum === 0) {
     randomNum = Math.random();
@@ -16,41 +20,84 @@ function computerChoice() {
   }
 }
 
-function humanChoice() {
-  return prompt("Rock, Paper, or Scissors?").toLowerCase().trim();
-}
 
 function playRound(humanChoice, computerChoice) {
-  console.log("You chose " + humanChoice);
-  console.log("Computer chose " + computerChoice);
-
   if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
+    winner = "";
   } else {
     const choices = ["rock", "paper", "scissors"];
 
     let num1 = choices.indexOf(humanChoice) + 1;
     let num2 = choices.indexOf(computerChoice) + 1;
 
-    if (num1 > num2 || num1 == 1 && num2 == 3) {
-      console.log("You win!");
+    if (num1 > num2 && !(num1 == 3 && num2 == 1) || num1 == 1 && num2 == 3) {
+      winner = "Player";
       humanScore++;
     } else {
-      console.log("Computer wins!");
+      winner = "Computer";
       computerScore++;
     }
   }
-  console.log(`Your score: ${humanScore}`); // backtick
-  console.log(`Computer score: ${computerScore}`);
-  console.log("");
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const human = humanChoice();
-    const computer = computerChoice();
-    playRound(human, computer);
+let btns = document.querySelectorAll("button");
+
+btns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    if (roundNum == 6) return;
+    humanChoice = e.target.textContent.toLowerCase();
+    computerChoice = generateComputerChoice();
+    playRound(humanChoice, computerChoice);
+    roundNum++;
+    update();
+  });
+});
+
+function update() {
+  
+  let round = document.querySelector("#round");
+  let pChoice = document.querySelector("#playerChoice");
+  let cChoice = document.querySelector("#computerChoice");
+  let pScore = document.querySelector("#playerScore");
+  let cScore = document.querySelector("#computerScore");
+  let w = document.querySelector(".winner");
+
+  pChoice.textContent = `Player chose: ${humanChoice}`;
+  cChoice.textContent = `Computer chose: ${computerChoice}`;
+  pScore.textContent = `Player score: ${humanScore}`;
+  cScore.textContent = `Computer score: ${computerScore}`;
+
+  if (winner === "") {
+    w.textContent = "It's a tie!";
+  }
+  else {
+    w.textContent = `${winner} wins!`;
+  }
+
+  if (roundNum < 6) {
+    round.textContent = `Round ${roundNum}`;
+  }
+  else {
+    let finalWinner = document.querySelector(".finalWinner");
+    let winningStatement = "";
+    
+    switch (humanScore > computerScore) {
+      case true:
+        winningStatement = "Player is the winner!";
+        break;
+      case false:
+        if (humanScore === computerScore) {
+          winningStatement = "It's a tie!";
+        }
+        else {
+          winningStatement = "Computer is the winner!";
+        }
+        break;
+    }
+
+    let statement = document.createElement("h3");
+    statement.textContent = "The game has ended. " + winningStatement;
+    finalWinner.appendChild(statement);
+
   }
 }
-
-playGame();
